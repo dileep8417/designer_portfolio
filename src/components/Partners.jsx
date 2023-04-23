2f74a1244e701b5c59dfb4e1e2b880c1941bbf2a
unknown <dileep8417@gmail.com>
2023-04-23 14:15:43 +0530

    Updated path

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,23 +1,13 @@
-import SlogDigital from '/designer_portfolio/assets/images/partners/company_1.png';
-import AdsGoat from '/designer_portfolio/assets/images/partners/company_2.png';
-import Internshala from '/designer_portfolio/assets/images/partners/company_3.png';
-import LTMindtree from '/designer_portfolio/assets/images/partners/company_4.png';
-import Lithan from '/designer_portfolio/assets/images/partners/company_5.png';
-import Lalasa from '/designer_portfolio/assets/images/partners/company_6.png';
-import Raha from '/designer_portfolio/assets/images/partners/company_7.png';
+import companyLogos from '../utils/partnerCompaniesLogo';
 import Marquee from 'react-fast-marquee';
 
 const Partners = () => {
     return (
         <div id="partners">
             <Marquee gradient={false} speed={60}>
-                <img className='partner_img' src={LTMindtree} alt="" />
-                <img className='partner_img' src={AdsGoat} alt="" />
-                <img className='partner_img' src={Lithan} alt="" />
-                <img className='partner_img' src={Lalasa} alt="" />
-                <img className='partner_img' src={SlogDigital} alt="" />
-                <img className='partner_img internshala' src={Internshala} alt="" />
-                <img className='partner_img' src={Raha} alt="" />
+                {companyLogos.map((logo, index) => {
+                    <img key={index} className='partner_img' src={logo} alt="logo" />
+                })}
             </Marquee>
         </div>
     );
---
+++ src/utils/partnerCompaniesLogo.js
@@ -0,0 +1,13 @@
+const imgResourcePath = '/designer_portfolio/assets/images/partners/';
+
+const companyLogos = [
+    imgResourcePath + 'company_1.png',
+    imgResourcePath + 'company_2.png',
+    imgResourcePath + 'company_3.png',
+    imgResourcePath + 'company_4.png',
+    imgResourcePath + 'company_5.png',
+    imgResourcePath + 'company_6.png',
+    imgResourcePath + 'company_7.png',
+];
+
+export default companyLogos;
